ed72762df881f00f3aab3be59ae0cde65f093693
DaisyKoome <dskoome@gmail.com>
2018-05-05 10:39:03 +0300

    This is Java Script code

---
+++ JS108.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.onload=arrayStuff;
+function arrayStuff(){
+function Rectangle(length,breadth,color){
+    
+var rectangle=new Rectangle(3.3,2.5,"Blue");
+console.log("Our rectangle has length "+rectangle.length);
+console.log("Our rectangle has breadth "+rectangle.breadth);
+console.log("Our rectangle has color "+rectangle.color);   
+};
+}
+
+
+    
+
