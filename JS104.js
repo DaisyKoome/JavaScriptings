3c36554957622494b9e057dc4cacb490f6869463
DaisyKoome <dskoome@gmail.com>
2018-05-05 10:31:45 +0300

    This is Java Script code

---
+++ JS104.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.onload=printX;
+var x=5;
+function printX() {
+    console.log("Inside printX:x= "+x);
+    x=x+10;
+    
+    }
+ 
+var x=x+10;
+function printAnotherX(){
+    console.log("Inside printAnotherX:X= "+x);
+}
+            
